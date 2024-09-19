4505242668e9fea796bd6ebeb7aa980ce5f8a9fc
Linards Ramza <linards.ramza@awave.com>
2024-09-19 13:12:23 +0300

    first commit

---
+++ wp-content/themes/wp-theme/assets/js/gutenberg-listing-block.js
@@ -0,0 +1,7 @@
+wp.blocks.registerBlockType('wp-theme/form-log-listing-block', {
+	title : 'Listing Block',
+	description : 'Block to list form log entries',
+	icon: 'dashicons-media-text',
+	category: 'design',
+	attributes: {},
+})
